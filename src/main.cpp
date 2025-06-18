3217e6ece22f83f8b104534bfb95a86b67edc5cc
Liam Swetnam <lvswetnam@gmail.com>
2025-06-18 14:55:07 -0500

    feat: Implement serial interaction

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,20 @@
 #include <receiver.hpp>
 
 void on_rf_receive(uint8_t* buf, uint8_t buf_size) {
-	// Need to reverse engineer the signals that the doorbell transmitter sends out to interpret
-	// and create conditional here. Will do soon.
+	// Need to reverse engineer the data that the doorbell transmitter sends out to interpret
+	// and create conditionals here. Will do soon.
+
+
+	const char* msg = "Doorbell rang!"; // Message we want displayed on the desktop
+	uint8_t msg_size = sizeof(msg);
+	
+	uint8_t bytes_to_send[msg_size+2];
+
+	bytes_to_send[0] = 0xAF; // Beginning bytes so the desktop application can verify
+	bytes_to_send[1] = 0xBE; // that this serial message is meant for it.
+
+	memcpy(bytes_to_send+0x02, msg, msg_size); // Offest dest by 2 to skip over the beginning bytes
+	Serial.write(bytes_to_send, sizeof(bytes_to_send));
 }
 
 rf_receiver* rcvr;
