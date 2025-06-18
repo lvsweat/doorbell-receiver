900ab42e8f9bb6bb7403a1099fa6d779ac7d5676
Liam Swetnam <lvswetnam@gmail.com>
2025-06-18 14:30:15 -0500

    chore: Add comments

--- lib/rf-driver/receiver.cpp
+++ lib/rf-driver/receiver.cpp
@@ -3,7 +3,7 @@
 rf_receiver::rf_receiver(uint16_t speed, uint8_t rxPin)
 {
     p_rf_driver = new RH_ASK(speed, rxPin, 5, 0);
-    p_on_receive = new OnReceiveCallback([](uint8_t*, uint8_t){});
+    p_on_receive = new OnReceiveCallback([](uint8_t*, uint8_t){}); // Initialize callback with empty function
 
     if(!initialize()) {
         Serial.println("Total driver initialization failure. Please check connections and restart.");
@@ -12,6 +12,7 @@ rf_receiver::rf_receiver(uint16_t speed, uint8_t rxPin)
 
 rf_receiver::~rf_receiver()
 {
+    // Deallocate heap allocations
     delete(p_rf_driver);
     delete(p_on_receive);
 }
@@ -20,7 +21,7 @@ bool rf_receiver::initialize()
 {
     int retry_count = 0;
     bool initialized = false;
-    while (!initialized && retry_count < 5) {
+    while (!initialized && retry_count < 5) { // RF ASK driver initialization attempt. Will try 5 times.
         initialized = p_rf_driver->init();
         if (!initialized) {
             Serial.println("Failed RF driver initilization. Trying again now.");
@@ -31,15 +32,15 @@ bool rf_receiver::initialize()
 }
 
 void rf_receiver::recv() {
-    uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+    uint8_t buf[RH_ASK_MAX_MESSAGE_LEN]; // Allocate byte array buffer with max possible size
     uint8_t bufLen = sizeof(buf);
 
-    if (p_rf_driver->recv(buf, &bufLen)) {
+    if (p_rf_driver->recv(buf, &bufLen)) { // Will be true when an internal checksum passes on the received data.
         p_rf_driver->printBuffer("Buffer received: ", buf, bufLen);
-        p_on_receive->operator()(buf, bufLen);
+        p_on_receive->operator()(buf, bufLen); // Call callback function
     }
 }
 
 void rf_receiver::set_on_receive(OnReceiveCallback onReceiveCallback) {
-    p_on_receive = new OnReceiveCallback(onReceiveCallback);
+    p_on_receive = new OnReceiveCallback(onReceiveCallback); // Allocate copy of passed callback function on heap
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,19 @@
 #include <receiver.hpp>
 
 void on_rf_receive(uint8_t* buf, uint8_t buf_size) {
-
+	// Need to reverse engineer the signals that the doorbell transmitter sends out to interpret
+	// and create conditional here. Will do soon.
 }
 
 rf_receiver* rcvr;
 
 void setup() {
-	Serial.begin(115200);
-	rcvr = new rf_receiver(2000, 4);
-	rcvr->set_on_receive(on_rf_receive);
+	Serial.begin(115200); // Begin serial comms
+	rcvr = new rf_receiver(2000, 4); // Create/allocate new receiver object on heap
+	rcvr->set_on_receive(on_rf_receive); // Override existing empty callback with our callback
 }
 
 void loop() {
-	rcvr->recv();
+	rcvr->recv();   // Constantly check for valid communications received. Will call on_rf_receive
+				    // when internal checksum pass in the wrapped driver receive function
 }
